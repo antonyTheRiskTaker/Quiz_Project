b89c05566cdaeab1493a6c8f0c2625b03245163e
sudais <127001sudais01@gmail.com>
2022-03-29 10:19:02 +0800

    new design and a test quiz area make sure to run through app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express();
+const { engine } = require("express-handlebars")
+
+app.engine("handlebars", engine({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+app.use(express.static('public'))
+
+const port = 3080
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.get("/signin", (req, res) => {
+    res.render('signin')
+})
+app.get("/admin", (req, res) => {
+    res.render('admin')
+})
+app.get("/quizarea", (req, res) => {
+    res.render('quizarea')
+})
+
+app.listen(port, () => {
+    console.log("server running on RTX3080")
+})
